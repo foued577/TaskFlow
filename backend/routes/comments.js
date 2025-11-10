83c15f25d3836ac07a71dcf9a4a108e8fe8c46fa
foued577 <144340690+foued577@users.noreply.github.com>
2025-11-10 17:11:06 +0100

    Update comments.js

--- backend/routes/comments.js
+++ backend/routes/comments.js
@@ -1,11 +1,24 @@
 const express = require('express');
 const router = express.Router();
-const { createComment, getComments, updateComment, deleteComment } = require('../controllers/commentController');
-const { protect } = require('../middleware/auth');
+const { protect } = require('../middleware/authMiddleware');
 
+const {
+  createComment,
+  getComments,
+  updateComment,
+  deleteComment
+} = require('../controllers/commentController');
+
+// Create comment
 router.post('/', protect, createComment);
+
+// Get all comments for a task
 router.get('/task/:taskId', protect, getComments);
+
+// Update comment
 router.put('/:id', protect, updateComment);
+
+// Delete comment
 router.delete('/:id', protect, deleteComment);
 
 module.exports = router;
