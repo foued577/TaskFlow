c998258df29e73ce40dd1d77f1e84531418eab00
foued577 <144340690+foued577@users.noreply.github.com>
2025-11-17 16:06:25 +0100

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -4,10 +4,10 @@ const UserSchema = new mongoose.Schema(
  {
    firstName: { type: String, required: true },
    lastName: { type: String, required: true },
-   email: { type: String, unique: true, required: true },
+   email: { type: String, required: true, unique: true },
    password: { type: String, required: true },
-   phone: { type: String },
-   bio: { type: String },
+   phone: String,
+   bio: String,
    teams: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Team' }],
    lastLogin: Date
  },
@@ -19,6 +19,6 @@ UserSchema.pre('save', async function (next) {
  next();
 });
 UserSchema.methods.matchPassword = async function (password) {
- return await bcrypt.compare(password, this.password);
+ return bcrypt.compare(password, this.password);
 };
 module.exports = mongoose.model('User', UserSchema);
