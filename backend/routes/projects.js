752cc071e8c5cca2ed6dafe5e9e3793e7074b775
foued577 <144340690+foued577@users.noreply.github.com>
2025-11-23 22:12:34 +0100

    Update projects.js

--- backend/routes/projects.js
+++ backend/routes/projects.js
@@ -1,26 +1,15 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
+const { createProject, getProjects, getProject, updateProject, deleteProject } = require('../controllers/projectController');
+const { protect } = require('../middleware/auth');
 
-const {
-  createProject,
-  getProjects,
-  getProject,
-  updateProject,
-  deleteProject,
-} = require("../controllers/projectController");
+router.route('/')
+  .get(protect, getProjects)
+  .post(protect, createProject);
 
-const { protect } = require("../middleware/auth");
-
-// Toutes les routes nécessitent un token
-router.use(protect);
-
-router.route("/")
-  .get(getProjects)
-  .post(createProject); // Admin seulement (géré dans le controller)
-
-router.route("/:id")
-  .get(getProject)
-  .put(updateProject)   // Admin seulement (géré dans le controller)
-  .delete(deleteProject); // Admin seulement
+router.route('/:id')
+  .get(protect, getProject)
+  .put(protect, updateProject)
+  .delete(protect, deleteProject);
 
 module.exports = router;
