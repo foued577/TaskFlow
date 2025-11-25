028cdc92f0fccdae094533a23e6ff3693bee6dab
foued577 <144340690+foued577@users.noreply.github.com>
2025-11-25 11:06:03 +0100

    Update teams.js

--- backend/routes/teams.js
+++ backend/routes/teams.js
@@ -1,15 +1,26 @@
 const express = require('express');
 const router = express.Router();
-const { createTeam, getTeams, getTeam, updateTeam, addMember, removeMember } = require('../controllers/teamController');
+
+const {
+  getTeams,
+  getTeam,
+  createTeam,
+  updateTeam,
+  addMember,
+  removeMember
+} = require('../controllers/teamController');
+
 const { protect } = require('../middleware/auth');
 
-router.route('/')
-  .get(protect, getTeams)
-  .post(protect, createTeam);
+// ====================================================
+// Toutes les routes nécessitent d’être authentifié
+// ====================================================
 
-router.route('/:id')
-  .get(protect, getTeam)
-  .put(protect, updateTeam);
+router.get('/', protect, getTeams);
+router.get('/:id', protect, getTeam);
+
+router.post('/', protect, createTeam);
+router.put('/:id', protect, updateTeam);
 
 router.post('/:id/members', protect, addMember);
 router.delete('/:id/members/:userId', protect, removeMember);
