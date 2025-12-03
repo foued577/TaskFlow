5c1c677ef5ae087ee17815a6ded41b7d837f7568
foued577 <144340690+foued577@users.noreply.github.com>
2025-12-03 16:29:12 +0100

    Update UsefulLink.js

--- backend/models/UsefulLink.js
+++ backend/models/UsefulLink.js
@@ -1,45 +1,32 @@
-UsefulLink.js :
-
+// backend/models/UsefulLink.js
 const mongoose = require("mongoose");
 
-const usefulLinkSchema = new mongoose.Schema({
-
-  title: {
-
-    type: String,
-
-    required: true,
-
-    trim: true
-
-  },
-
-  url: {
-
-    type: String,
-
-    required: true,
-
-  },
-
-  assignedTo: [{
-
-    type: mongoose.Schema.Types.ObjectId,
-
-    ref: "User"
-
-  }],
-
-  createdBy: {
-
-    type: mongoose.Schema.Types.ObjectId,
-
-    ref: "User",
-
-    required: true
-
-  }
-
-}, { timestamps: true });
+const usefulLinkSchema = new mongoose.Schema(
+{
+title: {
+type: String,
+required: true,
+trim: true,
+},
+url: {
+type: String,
+required: true,
+},
+assignedUsers: [
+{
+type: mongoose.Schema.Types.ObjectId,
+ref: "User",
+}
+],
+createdBy: {
+type: mongoose.Schema.Types.ObjectId,
+ref: "User",
+required: true,
+}
+},
+{
+timestamps: true,
+}
+);
 
 module.exports = mongoose.model("UsefulLink", usefulLinkSchema);
