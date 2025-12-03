97087c0e61f5a060027a35d1b320ad03641292b6
foued577 <144340690+foued577@users.noreply.github.com>
2025-12-03 15:35:55 +0100

    Create UsefulLink.js

---
+++ backend/models/UsefulLink.js
@@ -0,0 +1,45 @@
+UsefulLink.js :
+
+const mongoose = require("mongoose");
+
+const usefulLinkSchema = new mongoose.Schema({
+
+  title: {
+
+    type: String,
+
+    required: true,
+
+    trim: true
+
+  },
+
+  url: {
+
+    type: String,
+
+    required: true,
+
+  },
+
+  assignedTo: [{
+
+    type: mongoose.Schema.Types.ObjectId,
+
+    ref: "User"
+
+  }],
+
+  createdBy: {
+
+    type: mongoose.Schema.Types.ObjectId,
+
+    ref: "User",
+
+    required: true
+
+  }
+
+}, { timestamps: true });
+
+module.exports = mongoose.model("UsefulLink", usefulLinkSchema);
