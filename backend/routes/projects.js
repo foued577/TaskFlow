9e52385035d4cd68d878764c4885ce321d04162d
foued577 <144340690+foued577@users.noreply.github.com>
2026-01-28 09:59:41 +0100

    Update projects.js

--- backend/routes/projects.js
+++ backend/routes/projects.js
@@ -6,10 +6,8 @@ getProjects,
 getProject,
 createProject,
 updateProject,
-deleteProject,
-archiveProject,
-unarchiveProject
-} = require('../controllers/ProjectController'); // ✅ ICI (majuscule)
+deleteProject
+} = require('../controllers/projectController'); // ✅ FIX: projectController (minuscule)
 
 const { protect } = require('../middleware/auth');
 
@@ -23,9 +21,5 @@ router.get('/:id', protect, getProject);
 router.post('/', protect, createProject);
 router.put('/:id', protect, updateProject);
 router.delete('/:id', protect, deleteProject);
-
-// ✅ ✅ ✅ ARCHIVE / UNARCHIVE
-router.put('/:id/archive', protect, archiveProject);
-router.put('/:id/unarchive', protect, unarchiveProject);
 
 module.exports = router;
